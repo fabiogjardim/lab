d15cf7e530bfa0cb2cbaa5b4ed6e286f00ca1550
Fabio Jardim <fabiojardim@MacBook-Pro-de-Fabio.local>
2023-06-22 15:25:08 -0300

    modified:   start_containers.bat

--- start_containers.bat
+++ start_containers.bat
@@ -1,11 +1,10 @@
 mkdir .\postgres\postgres-db-volume\pg_stat
 mkdir .\postgres\postgres-db-volume\pg_tblspc
 
-echo "limpando containers"
+echo "limpando containers e iniciando ambiente"
 FOR /f "tokens=*" %%i IN ('docker ps -q -a') DO docker stop %%i
 FOR /f "tokens=*" %%i IN ('docker ps -q -a') DO docker rm %%i
 echo "inicinado containers"
-docker-compose up -d datanode
-
+docker-compose up -d 
  
 pause
