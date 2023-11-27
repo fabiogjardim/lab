b27d265bab22d1043c15b78be2b7f848cef83cb1
Fábio Jardim <fabiogjardim@gmail.com>
2023-11-27 19:08:15 -0300

    Update start_containers.bat

--- start_containers.bat
+++ start_containers.bat
@@ -5,6 +5,6 @@ echo "limpando containers e iniciando ambiente"
 FOR /f "tokens=*" %%i IN ('docker ps -q -a') DO docker stop %%i
 FOR /f "tokens=*" %%i IN ('docker ps -q -a') DO docker rm %%i
 echo "inicinado containers"
-docker-compose up -d datanomde namenode hue spark-master spark-worker hive trino minio
+docker-compose up -d datanode namenode hue spark-master spark-worker hive trino minio
  
 pause
